81604f093b962b15bad75d0783b6407457b713a1
xandstrike <52900614+xandstrike@users.noreply.github.com>
2021-07-01 21:37:09 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?PHP
+include('dbcon.php');
 function getRealIpAddr(){
  if ( !empty($_SERVER['HTTP_CLIENT_IP']) ) {
   // Check IP from internet.
@@ -12,5 +13,21 @@ function getRealIpAddr(){
  }
  return $ip;
 }
-echo getRealIpAddr();
+
+$ipadd = getRealIpAddr();
+
+	
+		$query2=mysqli_query($con,"select * from ipadd where ip='$ipadd'")or die(mysqli_error($con));
+		$count=mysqli_num_rows($query2);
+
+		if ($count>0)
+		{
+			echo "hellow";
+		}
+		else
+		{			
+			mysqli_query($con,"INSERT INTO ipadd(ip) VALUES('$ipadd')")or die(mysqli_error($con));
+
+			echo "hi";
+		}
 ?>
