960ec858f93cc844b097c67c94fb8a695c1d9a69
xandstrike <52900614+xandstrike@users.noreply.github.com>
2021-07-01 21:38:28 +0800

    Create dbcon.php

---
+++ dbcon.php
@@ -0,0 +1,12 @@
+<?php
+$con = mysqli_connect("z12itfj4c1vgopf8.cbetxkdyhwsb.us-east-1.rds.amazonaws.com","ohrrexcq1ss2mib6","ynm4wip0q33mwriw","hmuj4emtipvyjx0v");
+
+// Check connection
+if (mysqli_connect_errno())
+  {
+  echo "Failed to connect to MySQL: " . mysqli_connect_error();
+  }
+
+  date_default_timezone_set("Asia/Manila"); 
+?>
+
